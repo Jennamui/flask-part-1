63dd05b9e8971a0bd2a9fde371c6214b68cf806a
Jenna Mui <102179684+Jennamui@users.noreply.github.com>
2022-09-01 03:00:55 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return 'Hello world!'
+
+@app.route('/dashboard')
+def dashboard():
+    return 'This is the dashboard page'
+
+@app.route('/settings')
+def settings():
+    return 'This is the settings page'
+  
+  if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
