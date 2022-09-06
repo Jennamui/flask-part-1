33398ae2370d0e566bb70c5705b6817c986feecc
Jenna Mui <102179684+Jennamui@users.noreply.github.com>
2022-09-06 00:09:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def dashboard():
 @app.route('/settings')
 def settings():
     return 'This is the settings page'
-  
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
+  
